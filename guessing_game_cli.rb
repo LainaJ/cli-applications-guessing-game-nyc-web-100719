977a52f111d2ca8a6a2fd13a7c308bb98d749b7f
Laina Karosic <lainakarosic@gmail.com>
2019-09-29 19:17:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,6 +3,11 @@
 #this kind of file should only be concerned with the guessing game code itself
 def run_guessing_game
   random_number = rand(6) + 1
-  Puts "Guess a number between 1 and 6"
+  puts "Guess a number between 1 and 6"
   input = gets.chomp
+  if input == random_number
+    puts "You guessed the correct number!"
+  elsif if input !== random_number
+    puts "" 
+  end 
 end
